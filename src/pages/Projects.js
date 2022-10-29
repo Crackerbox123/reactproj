845774ce434a78d15be053643482b1b81fd0c595
Kal O <koestreicher4@gmail.com>
2022-10-29 06:37:43 -0500

    project images and cars

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,14 @@
 import React from 'react';
+// import individual projects
+
 import ProjectItem from "../components/ProjectItem";
-// import Img
+// import project images
+import RentalRecap from '../assets/RentalRecap.png'
+import Quiz from '../assets/Quiz.png'
+import NodeGenny from '../assets/NodeGenny.png'
+import WasteNot from '../assets/WasteNot.png'
+import Weather from '../assets/Weather.png'
+import TeamProfileGenny from '../assets/TeamProfileGenny.png'
 
 import "../styles/Projects.css";
 
@@ -9,7 +17,13 @@ function Projects() {
     <div className='projects'>
       <h1> My Personal Projects </h1>
       <div className='projectList'>   
-        <ProjectItem />
+        <ProjectItem name="Team Profile Generator" image={TeamProfileGenny}/>
+        <ProjectItem name="Rental Recap" image={RentalRecap}/>
+        <ProjectItem name="Quiz" image={Quiz}/>
+        <ProjectItem name="Node Readme Generator" image={NodeGenny}/>
+        <ProjectItem name="WasteNot" image={WasteNot}/>
+        <ProjectItem name="Weather App" image={Weather}/>
+        
 
       </div>
     </div>
